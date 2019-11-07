b44c41f5a329992793523b11fc94791e3601974f
Administrator <969990953@qq.com>
2019-11-07 16:32:31 +0800

    Initial commit

---
+++ src/test/java/cn/wx/dsc/DscApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wx.dsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
